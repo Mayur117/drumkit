f9e9bf38c13e7bb1bcabc96fe376f2423cded977
Mayur Thummar <41770235+Mayur117@users.noreply.github.com>
2022-03-03 10:48:50 +0530

    Digital drumkit

---
+++ index.js
@@ -0,0 +1,84 @@
+// detecting button press
+var numberOfDrumButtons = document.querySelectorAll("button").length;
+
+// loop through each button
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+    // add event listener to each button
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
+    });
+}
+
+// detecting keyboard press
+document.addEventListener("keydown", function (event) {
+
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "l":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+
+        default: console.log(buttonInnerHTML);
+
+    }
+
+}
+
+// add pressed class to drum button that is currently
+// being pressed or clicked
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    // timeout function allows pressed class
+    // to be removed after 0.1 seconds
+    setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    }, 100);
+
+  }
